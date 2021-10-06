00dc58b061bbeca8fea75f9b7cad156ce2630ffa
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2021-10-06 14:11:31 +0200

    [#2021.0025] Updates for proper package handling - FIX
    FIXED
     - The new JSON locations wouldn't always properly be loaded.

--- weather_provider_api/routers/weather/sources/knmi/knmi_factors.py
+++ weather_provider_api/routers/weather/sources/knmi/knmi_factors.py
@@ -12,13 +12,26 @@ import os
 import sys
 from pathlib import Path
 
-project_file = Path(os.getcwd()).parent.joinpath('var_maps').joinpath('arome_var_map.json')
-package_file = Path(sys.prefix).joinpath('var_maps').joinpath('arome_var_map.json')
 
-if project_file.exists():
-    file_to_use = project_file
-else:
-    file_to_use = package_file
+def _get_proper_file_location() -> Path:
+    file_name = 'arome_var_map.json'
+    var_map_folder = 'var_maps'
+
+    possible_main_folders = [
+        Path(os.getcwd()),
+        Path(os.getcwd()).parent,
+        Path(sys.prefix)
+    ]
+
+    for folder in possible_main_folders:
+        if folder.joinpath(var_map_folder).exists():
+            return folder.joinpath(var_map_folder).joinpath(file_name)
+
+    raise FileNotFoundError
+
+
+file_to_use = _get_proper_file_location()
+print("FILE TO USE:", file_to_use)
 
 with open(file_to_use, "r") as _f:
     arome_factors = json.load(_f)
