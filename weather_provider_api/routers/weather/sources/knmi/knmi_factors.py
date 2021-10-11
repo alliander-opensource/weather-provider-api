e075d0dbe527ec4c914c425fb076ad6f64a7b513
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2021-10-11 08:31:17 +0200

    [#2021.0028] Removed duplicate code that caused quality gate fail

--- weather_provider_api/routers/weather/sources/knmi/knmi_factors.py
+++ weather_provider_api/routers/weather/sources/knmi/knmi_factors.py
@@ -8,30 +8,10 @@
 # factor name mapping
 
 import json
-import os
-import sys
-from pathlib import Path
 
+from weather_provider_api.routers.weather.utils.file_helpers import get_var_map_file_location
 
-def _get_proper_file_location() -> Path:
-    file_name = 'arome_var_map.json'
-    var_map_folder = 'var_maps'
-
-    possible_main_folders = [
-        Path(os.getcwd()),
-        Path(os.getcwd()).parent,
-        Path(sys.prefix)
-    ]
-
-    for folder in possible_main_folders:
-        if folder.joinpath(var_map_folder).exists():
-            return folder.joinpath(var_map_folder).joinpath(file_name)
-
-    raise FileNotFoundError
-
-
-file_to_use = _get_proper_file_location()
-print("FILE TO USE:", file_to_use)
+file_to_use = get_var_map_file_location('arome_var_map.json')
 
 with open(file_to_use, "r") as _f:
     arome_factors = json.load(_f)
