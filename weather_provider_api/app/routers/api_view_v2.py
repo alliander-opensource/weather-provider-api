bec6f4b8f098611bed4028320dbb2c5f36e120c0
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2021-12-02 16:19:55 +0100

    Initial Commit for V3.x
    (earlier development for this version will be housed as branching solutions from this trunk branch)

--- weather_provider_api/app/routers/api_view_v2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# SPDX-FileCopyrightText: 2019-2021 Alliander N.V.
-#
-# SPDX-License-Identifier: MPL-2.0
-from fastapi import APIRouter
-
-app = APIRouter()
