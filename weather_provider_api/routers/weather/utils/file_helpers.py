3c78a2976728747e13d0a263bc52ab6e48ab806c
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2022-11-02 13:38:46 +0100

    [2022-11-02 13:19] Fixed image issues created by package updates (#80)
    CHANGES:
     - Version increment to 2.30.0
    
    FIXES:
     - Fixed version fetching which broke image functionality
     - Fixed the main execution function which didn't properly work due to the 'app' object no longer being properly accessible.
    Signed-off-by: Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
    
    Signed-off-by: Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>

--- weather_provider_api/routers/weather/utils/file_helpers.py
+++ weather_provider_api/routers/weather/utils/file_helpers.py
@@ -33,7 +33,7 @@ def get_var_map_file_location(filename: str) -> Path:
     possible_main_folders = [
         Path(os.getcwd()),  # Running from main folder
         Path(os.getcwd()).parent,  # Running from weather_provider_api folder or scripts
-        Path(site.getsitepackages()[-1]),   # Running as package
+        Path(site.getsitepackages()[-1]),  # Running as package
     ]
 
     for folder in possible_main_folders:
@@ -41,6 +41,5 @@ def get_var_map_file_location(filename: str) -> Path:
         if possible_var_map_folder.exists():
             logger.info(f'"var_maps" folder was found at: {possible_var_map_folder}')
             return possible_var_map_folder.joinpath(filename)
-
 
     raise FileNotFoundError
