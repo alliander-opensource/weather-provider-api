1055f68d66518f0ab9de6fdf2d4652cd54286d71
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2023-11-01 08:33:52 +0100

    [2023-11-01 08:33] Server setting adjustments
    Signed-off-by: Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>

--- weather_provider_api/versions/v1.py
+++ weather_provider_api/versions/v1.py
@@ -6,14 +6,14 @@
 
 from fastapi import FastAPI
 
-from weather_provider_api.config import APP_CONFIG
+from weather_provider_api.config import APP_CONFIG, APP_SERVER
 from weather_provider_api.routers.weather.api_view_v1 import app as weather_router
 
 app = FastAPI(
     version=APP_CONFIG["api_v1"]["implementation"],
     title="Weather API (v1)",
     root_path="/api/v1",
-    servers=[{"url": "/api/v1"}],
+    servers=[{"url": f"{APP_SERVER}/api/v1"}],
     description="The v1 endpoint interface for the Weather Provider API",
     contact={"name": APP_CONFIG["maintainer"]["name"], "email": APP_CONFIG["maintainer"]["email_address"]},
 )
