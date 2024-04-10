9953ceb34ebefb079074cb18e4a3ecd6a9003c6a
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2024-04-10 07:38:33 +0200

    [2023-04-10 07:35] Micro patch
    Signed-off-by: Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>

--- src/weather_provider_api/core/utils/file_transformers.py
+++ src/weather_provider_api/core/utils/file_transformers.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+#  !/usr/bin/env python
+#  -*- coding: utf-8 -*-
+#
+#  SPDX-FileCopyrightText: 2019-2024 Alliander N.V.
+#  SPDX-License-Identifier: MPL-2.0
+
 #  -------------------------------------------------------
 #  SPDX-FileCopyrightText: 2019-2024 Alliander N.V.
 #  SPDX-License-Identifier: MPL-2.0
@@ -7,13 +13,11 @@
 
 from pathlib import Path
 
-from loguru import logger
-
 _NEEDED_CONFIG_FILES = ["config.toml"]
 
 
 def get_main_project_folder() -> Path:
-    """Identify and return the main project folder for the Weather Provider Libraries."""
+    """Identify and return the main project folder for the Weather Provider API."""
     presumed_main_project_folder = Path(__file__).parent.parent.parent
 
     if (
@@ -21,7 +25,7 @@ def get_main_project_folder() -> Path:
         or not presumed_main_project_folder.is_dir()
     ):
         raise NotADirectoryError(
-            f"Main project folder for Weather Provider Libraries not found at: {presumed_main_project_folder}"
+            f"Main project folder for Weather Provider API not found at: {presumed_main_project_folder}"
         )
 
     if (
@@ -29,7 +33,7 @@ def get_main_project_folder() -> Path:
         or not presumed_main_project_folder.joinpath("core").is_dir()
     ):
         raise NotADirectoryError(
-            "Main project folder for Weather Provider Libraries is missing the 'data_classes' subfolder."
+            "Main project folder for Weather Provider API is missing the 'core' subfolder."
             "As such, it cannot be the main project folder."
             f"Main project folder for Weather Provider Libraries not found at: {presumed_main_project_folder}"
         )
@@ -38,21 +42,23 @@ def get_main_project_folder() -> Path:
 
 
 def get_main_config_folder() -> Path:
-    """Identify and return the main configuration folder for the Weather Provider Libraries."""
+    """Identify and return the main configuration folder for the Weather Provider API."""
     list_of_possible_config_folder_locations = [
         get_main_project_folder().parent.parent.joinpath("wpa_config"),
         Path("./wpa_config"),
         Path("/wpa_config"),
-        Path("/etc/wpa_config"),
+        Path("/etc/wpa_config")
     ]
     for location in list_of_possible_config_folder_locations:
         if location.exists() and location.is_dir():
+            all_files_present = True
             for filename in _NEEDED_CONFIG_FILES:
                 file = location.joinpath(filename)
                 if not file.exists() or not file.is_file():
-                    break
-            return location
-    # TODO: Fix
+                    all_files_present = False
+            if all_files_present:
+                return location
+
     raise FileNotFoundError(
-        "No valid configuration folder found for the Weather Provider Libraries."
+        "No valid configuration folder found for the Weather Provider API."
     )
