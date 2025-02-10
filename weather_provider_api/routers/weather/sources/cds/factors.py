acea8cf8a0e97bbf0354187b5c65bc863b78979a
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2025-02-10 09:50:46 +0100

    [2025-02-10 09:47] Micropatch for requesting specific fields

--- weather_provider_api/routers/weather/sources/cds/factors.py
+++ weather_provider_api/routers/weather/sources/cds/factors.py
@@ -16,7 +16,10 @@ file_to_use_era5sl = get_var_map_file_location("era5sl_var_map.json")
 file_to_use_era5land = get_var_map_file_location("era5land_var_map.json")
 
 with open(file_to_use_era5sl, "r") as _f:
-    era5sl_factors = json.load(_f)
+    era5sl_factors: dict = json.load(_f)
+    for factor in ["mwd", "mwp", "swh"]:
+        era5sl_factors.pop(factor)
+
 
 with open(file_to_use_era5land, "r") as _f:
     era5land_factors = json.load(_f)
