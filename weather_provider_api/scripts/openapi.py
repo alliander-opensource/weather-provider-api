43db43c96619d1445d4d7be738bc57b81f4f28d5
Derk Weijers <derkweijers@users.noreply.github.com>
2026-01-19 15:32:39 +0100

    feat: add feature to save the openapi to disk (#158)
    * feat: add feature to save the openapi to disk
    
    Signed-off-by: Derk Weijers <derk.weijers@alliander.com>
    
    * chore: add tests as well
    
    Signed-off-by: Derk Weijers <derk.weijers@alliander.com>
    
    * chore: cleanup
    
    Signed-off-by: Derk Weijers <derk.weijers@alliander.com>
    
    ---------
    
    Signed-off-by: Derk Weijers <derk.weijers@alliander.com>

---
+++ weather_provider_api/scripts/openapi.py
@@ -0,0 +1,12 @@
+import json
+
+from weather_provider_api.core.application import WPLA_APPLICATION
+from weather_provider_api.versions.v2 import app as v2_app
+
+
+
+def generate_openapi_spec():
+    """Generate OpenAPI specification and save it to a file."""
+    spec = v2_app.openapi()
+    with open("openapi.json", "w") as f:
+        f.write(json.dumps(spec))
