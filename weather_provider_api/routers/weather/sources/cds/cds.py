edbe9c16254530567be969fea8117b2711951711
rflinnenbank <58594297+rflinnenbank@users.noreply.github.com>
2026-06-03 14:36:37 +0200

    feature: Added ERA5 Land to default CDS models
    Also added a Dependabot file and patched an erroneous pylint setting.
    
    Signed-off-by: rflinnenbank <58594297+rflinnenbank@users.noreply.github.com>

--- weather_provider_api/routers/weather/sources/cds/cds.py
+++ weather_provider_api/routers/weather/sources/cds/cds.py
@@ -3,15 +3,16 @@
 
 from weather_provider_api.routers.weather.base_models.model import WeatherModelBase
 from weather_provider_api.routers.weather.base_models.source import WeatherSourceBase
+from weather_provider_api.routers.weather.sources.cds.models.era5land import ERA5LandModel
 from weather_provider_api.routers.weather.sources.cds.models.era5sl import ERA5SLModel
 
 
 class CDS(WeatherSourceBase):
+    """Climate Data Store (CDS) weather source implementation."""
+
     def __init__(self):
         """Initialize the CDS source and set up its models."""
-        model_instances: list[WeatherModelBase] = [
-            ERA5SLModel(),
-        ]
+        model_instances: list[WeatherModelBase] = [ERA5SLModel(), ERA5LandModel()]
 
         super().__init__(
             source_id="cds",
